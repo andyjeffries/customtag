155db73ad19e1d5f21464364647141a46088e754
Andy Jeffries <andy@andyjeffries.co.uk>
2023-06-07 10:43:22 +0100

    Initial gem build

---
+++ lib/custom_tag.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "custom_tag/version"
+
+module CustomTag
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/custom_tag/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module CustomTag
+  VERSION = "0.1.0"
+end
