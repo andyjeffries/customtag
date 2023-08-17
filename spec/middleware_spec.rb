c8dfd575d2fd49788762f43aacec654166e781ff
Andy Jeffries <andy@andyjeffries.co.uk>
2023-08-17 09:06:45 +0100

    Fix failing specs

--- spec/middleware_spec.rb
+++ spec/middleware_spec.rb
@@ -22,14 +22,14 @@ RSpec.describe CustomTag::Middleware do
     app = ->(env) { [200, {"Content-Type" => "text/html"}, ["<!DOCTYPE html><html><body><ignore>me</ignore></body></html>"]] }
     middleware = CustomTag::Middleware.new(app)
 
-    expect(middleware.call(env_for("http://example.com"))).to eq([200, {"Content-Type" => "text/html"}, ["<!DOCTYPE html>\n<html><body><ignore>me</ignore></body></html>\n"]])
+    expect(middleware.call(env_for("http://example.com"))).to eq([200, {"Content-Length"=>"62", "Content-Type" => "text/html"}, ["<!DOCTYPE html>\n<html><body><ignore>me</ignore></body></html>\n"]])
   end
 
   it "returns returns original body with replaced tags if found" do
     app = ->(env) { [200, {"Content-Type" => "text/html"}, ["<!DOCTYPE html><html><body><replace>me</replace></body></html>"]] }
     middleware = CustomTag::Middleware.new(app)
 
-    expect(middleware.call(env_for("http://example.com"))).to eq([200, {"Content-Type" => "text/html"}, ["<!DOCTYPE html>\n<html><body><div class=\"text-red-500\">me</div></body></html>\n"]])
+    expect(middleware.call(env_for("http://example.com"))).to eq([200, {"Content-Length"=>"77", "Content-Type" => "text/html"}, ["<!DOCTYPE html>\n<html><body><div class=\"text-red-500\">me</div></body></html>\n"]])
   end
 
   def env_for url, opts={}
